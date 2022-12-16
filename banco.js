135671bc854a619b093f3be8f441e491ae725cba
Matheus Fantini Teixeira <88668414+MFantiniT@users.noreply.github.com>
2022-12-16 14:45:06 -0300

    Add files via upload

---
+++ banco.js
@@ -0,0 +1,56 @@
+const read = require('readline-sync')
+class Cliente {
+    constructor(cliente, idade, chavePix, saldo) {
+        this.cliente = cliente,
+            this.idade = idade,
+            this.chavePix = chavePix,
+            this.saldo = saldo
+    }
+    mostrarDados() {
+        console.log(`Nome: ${this.cliente}
+        ---------------------
+        Idade: ${this.idade}
+        ---------------------
+        Chave Pix: ${this.chavePix}
+        ---------------------
+        Saldo: ${this.saldo}
+        ---------------------`)
+    }
+    adicionarSaldo(valor) {
+        this.saldo += valor
+        console.log('Seu saldo agora é de ' + this.saldo + 'Reais')
+    }
+    transferenciaTed() {
+        let conta = read.question(`Digite o numero da conta na qual deseja fazer a transferencia.`)
+        let valor = read.question('Qual valor deseja transferir?')
+        this.saldo -= valor
+        console.log(`você transferiu ${valor} reais, para a conta ${conta}
+                    seu saldo agora é de ${this.saldo}`)
+    }
+    transferenciaPix() {
+        
+        let pixtransferencia = (read.question("digite a chave pix na qual deseja enviar o valor"))
+        let valor = read.question('qual valor deseja enviar?')
+        this.saldo -= valor
+        console.log(`você fez um pix no valor de ${valor} reais, para a chave ${pixtransferencia}
+                    seu saldo agora é de: ${saldo} reais`)
+    }
+
+
+    fazerTransferencia() {
+        let opcao = read.question(`digite 1 para transferencia TED
+        digite 2 para transferencia PIX`)
+        if (opcao === '1') {
+            this.transferenciaTed()
+        }
+        if (opcao === '2') {
+            this.transferenciaPix()
+        }
+
+    }
+}
+
+matheus = new Cliente('Matheus Fantini Teixeira', 27, 09600978999, 3200)
+
+
+console.log(matheus.fazerTransferencia())
